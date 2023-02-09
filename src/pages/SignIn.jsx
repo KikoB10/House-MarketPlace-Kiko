c56f01b29a200b71dfb0c383f39ae226473c3ca7
KikoB10 <burten.kiko@gmail.com>
2023-02-08 22:32:44 -0600

    SignIn set up and links to Profile which will currently display the displayName (userName) else 'Not Logged In'

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,6 +2,11 @@ import { React, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithEmailANdPassword,
+} from 'firebase/auth';
 
 function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -19,6 +24,24 @@ function SignIn() {
       [e.target.id]: e.target.value,
     }));
   };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="pageContainer">
@@ -26,7 +49,7 @@ function SignIn() {
           <p>Welcome Back</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="email"
             className="emailInput"
