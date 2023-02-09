273e07e20f0093e83e272aa5f39141b0f71b2891
KikoB10 <burten.kiko@gmail.com>
2023-02-09 17:36:21 -0600

    Category page created, explore page updated so that we can start to show listings when images clicked.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -21,10 +21,10 @@ function Explore() {
             />
             <p className="exploreCategoryName">Places for rent</p>
           </Link>
-          <Link to="/category/sell">
+          <Link to="/category/sale">
             <img
               src={sellCategoryImage}
-              alt="rent"
+              alt="sell"
               className="exploreCategoryImg"
             />
             <p className="exploreCategoryName">Places for sale</p>
