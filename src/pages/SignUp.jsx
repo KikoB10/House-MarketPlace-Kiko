24e238d0790792d47c9eef14705403a825536de1
KikoB10 <burten.kiko@gmail.com>
2023-02-09 15:49:42 -0600

    Google OAuth implemented. Everything is functional but the icon does not display properly. need to revisit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import {
 } from 'firebase/auth';
 import { db } from '../firebase.config';
 import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
+import OAuth from '../components/OAuth';
 
 function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -105,7 +106,9 @@ function SignUp() {
             </button>
           </div>
         </form>
-        {/* Google OAth */}
+
+        <OAuth />
+
         <Link to="/sign-in" className="registerLink">
           Sign In Instead
         </Link>
